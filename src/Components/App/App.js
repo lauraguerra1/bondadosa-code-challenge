cd82eeb0e474c0fb4183b2a3185f9c23f42f4898
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-27 12:45:29 -0500

    Add count to cart

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getItem } from '../../utils/apiCalls';
 import { NavLink, Routes, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Cart from '../Cart/Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     const callAPI = async () => {
       try {
@@ -24,7 +25,16 @@ function App() {
       <header>
         <h1 id='title'>VIRTUAL GROCER</h1>
         <nav>
-          <NavLink to='/cart'>CART</NavLink>
+          <NavLink to='/cart' className='cartBtn'>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='icon'>
+              <path strokeLinecap='round' strokeLinejoin='round' d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z' />
+            </svg>
+            {cart.length > 0 && (
+              <div class='cartCount'>
+                <p aria-label='Cart Quantity'>{cart.length}</p>
+              </div>
+            )}
+          </NavLink>
         </nav>
       </header>
       <main>
