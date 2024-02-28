bb7fb2b09eb17d2c0099bdb4bb5a42248104cea6
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-27 22:03:00 -0500

    Carry search bar value across screen sizes

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './SearchBar.css'
 
-const SearchBar = ({className}) => {
+const SearchBar = ({className, searchTerm, updateSearchTerm}) => {
   return (
     <form className={className}>
-      <input type='search' placeholder='Search for grocery items' />
+      <input type='search' placeholder='Search for grocery items' value={searchTerm} onChange={(e) => updateSearchTerm(e.target.value)} />
       <button className='search-btn'>
         <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='icon'>
           <path strokeLinecap='round' strokeLinejoin='round' d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z' />
@@ -15,8 +15,10 @@ const SearchBar = ({className}) => {
   )
 }
 
-
 SearchBar.propTypes = {
-  className: PropTypes.string.isRequired
+  className: PropTypes.string.isRequired, 
+  searchTerm: PropTypes.string.isRequired, 
+  updateSearchTerm: PropTypes.func.isRequired
 };
+
 export default SearchBar
