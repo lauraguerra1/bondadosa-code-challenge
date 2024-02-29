741dcad9ab924f75e3ae1432241a1ef3c64fe723
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-29 14:55:12 -0500

    Pull results from api and allow loading more results

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { getItem } from '../../utils/apiCalls';
+import { getItem, getMoreItems } from '../../utils/apiCalls';
 import './SearchResults.css'
 import brokenImg from '../../images/broken-img.png';
-import exampleResults from '../../utils/edamam_response.json'
 
 const SearchResults = ({ searchParams }) => {
-  const [searchResults, setSearchResults] = useState(exampleResults.hints);
+  const [searchResults, setSearchResults] = useState([]);
+  const [nextApi, setNextApi] = useState('')
+
+  const callAPI = async (apiCall, argument, type) => {
+    try {
+      const data = await apiCall(argument);
+      type === 'new' ? setSearchResults(data.hints) : setSearchResults(prev => [...prev, ...data.hints])
+      if(data["_links"].next.href) setNextApi(data["_links"].next.href)
+    } catch (error) {
+      console.log(error);
+      setSearchResults([])
+    }
+  };
 
   useEffect(() => { 
-
-
-    const callAPI = async () => {
-      try {
-        const data = await getItem(searchParams.get('q') || '');
-        setSearchResults(data.hints)
-      } catch (error) {
-        console.log(error);
-        setSearchResults([])
-      }
-    };
-
-    // callAPI();
+    callAPI(getItem, (searchParams.get('q') || ''), 'new');
   }, [searchParams])
 
   if (!searchParams.get('q')) {
@@ -35,15 +34,16 @@ const SearchResults = ({ searchParams }) => {
 
   return (
     <section className='results-page'>
-    <p className='semi-bold'>Showing {searchResults.length} result{searchResults.length === 1 ? '' : 's'} for {searchParams.get('q') || ''} </p>
+    <p className='semi-bold'>Showing {uniqueResults.length} result{uniqueResults.length === 1 ? '' : 's'} for {searchParams.get('q') || ''} </p>
     <div className={`results ${uniqueResults.length % 2 === 0 ? 'even-length' : 'odd-length'}`}>
-      {uniqueResults.map((item, index) => 
-        <div className={`single-result ${index === uniqueResults.length - 1 ? 'last' : index === uniqueResults.length - 2 ? 'second-last' : index === uniqueResults.length - 3 ? 'third-last' : ''} ${index % 2 === 0 ? '' : 'odd'} ${index % 3 === 0 ? 'every-third' : ''}`} key={item.food.foodId}>
-          <img src={item.food.image || brokenImg} alt={item.food.label} />
-          <p className='semi-bold'>{item.food.label}</p>
-        </div>
-      )}
-    </div>
+        {uniqueResults.map((item, index) => 
+          <div className={`single-result ${index === uniqueResults.length - 1 ? 'last' : index === uniqueResults.length - 2 ? 'second-last' : index === uniqueResults.length - 3 ? 'third-last' : ''} ${index % 2 === 0 ? '' : 'odd'} ${index % 3 === 0 ? 'every-third' : ''}`} key={item.food.foodId}>
+            <img src={item.food.image || brokenImg} alt={item.food.label} />
+            <p className='semi-bold'>{item.food.label}</p>
+          </div>
+        )}
+      </div>
+      {nextApi.length > 0 && <button className='view-more' onClick={() => callAPI(getMoreItems, nextApi, 'add')}>View More Items</button>}
     </section>
   )
 }
