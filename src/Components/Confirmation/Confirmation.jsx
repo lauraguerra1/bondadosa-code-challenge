8fc32f3260f527b9312840d593fea610233a4e8a
Laura Guerra <l.garciaguerra1@gmail.com>
2024-03-03 22:03:47 -0500

    Create initial conditions for confirmation page

--- src/Components/Confirmation/Confirmation.jsx
+++ src/Components/Confirmation/Confirmation.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const Confirmation = () => {
   const { orderId } = useParams();
-  
+  const [loading, setLoading] = useState(true);
+  const [purchasedOrder, setPurchasedOrder] = useState(null);
+
   useEffect(() => {
-    const purchasedOrder = JSON.parse(localStorage.getItem(orderId));
-    console.log('purcahsed order', purchasedOrder)
-  }, [])
+    setPurchasedOrder(JSON.parse(localStorage.getItem(orderId)) || null);
+    setLoading(false);
+  }, []);
+
   return (
-    <div>Confirmation {orderId}</div>
+    <div>
+      {loading ? <p>loading...</p> : purchasedOrder ? 
+        <h2>Thank you for your order! {purchasedOrder.orderId}</h2>
+        : <p>We couldnt find that order </p>
+      }
+    </div>
   )
 }
 
